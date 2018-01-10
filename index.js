73dca24f46b6eebfff2a1dbbb7291b9d9fbe9885
Eric Chrobak <eric@ericchrobak.com>
2018-01-10 18:23:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ function add2(n) {
   return n + two
 }
 
-function funkyFunction() {
-  return function funky() {
+var funkyFunction = function() {
+  return function() {
     return "FUNKY!"
   }
 }
